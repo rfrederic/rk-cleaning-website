eeb21187ed8454a82280ef6b8e88b3be775a6cca
RigaudsonFrederic <frigaudson1997@gmail.com>
2026-02-08 23:56:15 -0500

    new update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
         ☰
       </button>
 
-      <ul className={`nav__links ${isOpen ? "nav__links--open" : ""}`}>
+      <ul className={`nav__links ${isOpen ? "open" : ""}`}>
         <li>
           <Link to="/" onClick={() => setIsOpen(false)}>
             Home
